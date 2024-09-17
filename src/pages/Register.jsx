8311ec8bc4adbe25d844e618cf0d57cc293a6cee
gokcekemaloglu <gokce.kemaloglu@gmail.com>
2024-09-17 12:40:47 +0300

    Initialize project setup - Installed Formik and Yup - Added Formik form to Register page - Set initial state and parameters for Formik structure - Integrated Material UI TextFields - Created Register form

---
+++ src/pages/Register.jsx
@@ -0,0 +1,163 @@
+import Container from "@mui/material/Container";
+import Typography from "@mui/material/Typography";
+import Avatar from "@mui/material/Avatar";
+import LockIcon from "@mui/icons-material/Lock";
+import image from "../assets/regi.avif";
+import Grid from "@mui/material/Grid";
+import { Link } from "react-router-dom";
+import { Box, Button, TextField } from "@mui/material";
+import AuthHeader from "../components/AuthHeader";
+import AuthImage from "../components/AuthImage";
+import { Form, Formik } from "formik";
+import * as Yup from 'yup';
+
+const SignupSchema = Yup.object().shape({
+  username: Yup.string().required("Bu alan zorunludur!").min(3, "Username en az 3 karakter olmalıdır!"),
+  firstName: Yup.string()
+    .min(2, 'Too Short!')
+    .max(50, 'Too Long!')
+    .required('Required'),
+  lastName: Yup.string()
+    .min(2, 'Too Short!')
+    .max(50, 'Too Long!')
+    .required('Required'),
+  email: Yup.string().email('Invalid email').required('Required'),
+  password: Yup.string().required().min(8).matches(/\d+/,"En az bir rakam içermelidir!").matches(/[a-z]/,"En az bir küçük harf içermelidir!").matches(/[A-Z]/,"En az bir büyük harf içermelidir!").matches(/[@$%&?!*]+/,"(@$%&?!*) özel karakterlerinden en az bir tanesini içermelidir!")
+});
+
+const Register = () => {
+  return (
+    <Container maxWidth="lg">
+      <Grid
+        container
+        justifyContent="center"
+        direction="row-reverse"
+        rowSpacing={{ sm: 3 }}
+        sx={{
+          height: "100vh",
+          p: 2,
+        }}
+      >
+        <AuthHeader />
+
+        <Grid item xs={12} sm={10} md={6}>
+          <Avatar
+            sx={{
+              backgroundColor: "secondary.light",
+              m: "auto",
+              width: 40,
+              height: 40,
+            }}
+          >
+            <LockIcon size="30" />
+          </Avatar>
+          <Typography
+            variant="h4"
+            align="center"
+            mb={2}
+            color="secondary.light"
+          >
+            Register
+          </Typography>
+
+          <Formik
+            initialValues = {{
+              username: "",
+              password: "",
+              email: "",
+              firstName: "",
+              lastName: "",
+            }}
+            validationSchema={SignupSchema}
+            onSubmit={values => {
+              // same shape as initial values
+              console.log(values);
+            }}
+          >
+            {({
+              values,
+              errors,
+              touched,
+              handleChange,
+              handleBlur,
+              // handleSubmit,
+              isSubmitting,
+              /* and other goodies */
+            }) => (
+              <Form>
+                <Box sx={{display:"flex", flexDirection:"column", gap:2}}>
+                  <TextField
+                    name="username"
+                    label="Username"
+                    type="text"
+                    value={values.username}
+                    onChange={handleChange}
+                    onBlur={handleBlur}
+                    error={touched.username && Boolean(errors.username)}
+                    helperText={touched.username && errors.username}
+                  />
+                  <TextField
+                    id="firstName"
+                    name="firstName"
+                    label="FirstName"
+                    type="text"
+                    value={values.firstName}
+                    onChange={handleChange}
+                    onBlur={handleBlur}
+                    error={touched.firstName && Boolean(errors.firstName)}
+                    helperText={touched.firstName && errors.firstName}
+                  />
+                  <TextField
+                    id="lastName"
+                    name="lastName"
+                    label="LastName"
+                    type="text"
+                    value={values.lastName}
+                    onChange={handleChange}
+                    onBlur={handleBlur}
+                    error={touched.lastName && Boolean(errors.lastName)}
+                    helperText={touched.lastName && errors.lastName}
+                  />
+                  <TextField
+                    id="email"
+                    name="email"
+                    label="Email"
+                    type="email"
+                    value={values.email}
+                    onChange={handleChange}
+                    onBlur={handleBlur}
+                    error={touched.email && Boolean(errors.email)}
+                    helperText={touched.email && errors.email}
+                  />
+                  <TextField
+                    id="password"
+                    name="password"
+                    label="password"
+                    type="password"
+                    value={values.password}
+                    onChange={handleChange}
+                    onBlur={handleBlur}
+                    error={touched.password && Boolean(errors.password)}
+                    helperText={touched.password && errors.password}
+                  />
+                  <Button type="submit" variant="contained" disabled={isSubmitting}>
+                    {isSubmitting ? "Loading..." : "Sign Up"}
+                  </Button>
+                </Box>
+              </Form>
+            )}
+            
+          </Formik>
+
+          <Box sx={{ textAlign: "center", mt: 2, color:"secondary.main" }}>
+            <Link to="/">Already have an account? Sign in</Link>
+          </Box>
+        </Grid>
+
+        <AuthImage image={image} />
+      </Grid>
+    </Container>
+  );
+};
+
+export default Register;
