70fc21f2a23d5bde7021bf2cfdcd33e27d131812
gokcekemaloglu <gokce.kemaloglu@gmail.com>
2024-09-18 12:08:34 +0300

    Added imports and middleware configuration for redux-persist in store.jsx

--- src/app/store.jsx
+++ src/app/store.jsx
@@ -1,6 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "../features/authSlice";
-import { persistStore, persistReducer } from 'redux-persist'
+import { persistStore, persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER, } from 'redux-persist'
 import storage from 'redux-persist/lib/storage'
 
 //* redux-persist
@@ -17,6 +23,12 @@ const store = configureStore({
     // auth: authReducer,
     auth: persistedReducer,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+  }),
   devTools: process.env.NODE_ENV !== "production",
 });
 
