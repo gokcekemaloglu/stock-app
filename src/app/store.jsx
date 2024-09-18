6b3830a47755ed910760389c07701acd326a0c63
gokcekemaloglu <gokce.kemaloglu@gmail.com>
2024-09-18 12:08:04 +0300

    Added redux-persist to the project, updated package.json and pnpm-lock.yaml, and modified App.jsx and store.jsx to integrate redux-persist

--- src/app/store.jsx
+++ src/app/store.jsx
@@ -1,10 +1,26 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "../features/authSlice";
+import { persistStore, persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
 
+//* redux-persist
+const persistConfig = {
+  key: 'root',
+  storage,
+}
+
+const persistedReducer = persistReducer(persistConfig, authReducer)
+
+//! reduxtoolkit store
 const store = configureStore({
   reducer: {
-    auth: authReducer,
+    // auth: authReducer,
+    auth: persistedReducer,
   },
   devTools: process.env.NODE_ENV !== "production",
 });
+
+//! redux-persist
+export let persistor = persistStore(store)
+
 export default store;
