822ec94e2d15abecea3c6b498ed6e1f00114a5fd
gokcekemaloglu <gokce.kemaloglu@gmail.com>
2024-09-19 12:28:08 +0300

    Upload @mui/x-data-grid, update @mui/material. Created ProductModal, update Products and import ProductModal to Products Created PurchaseModal, update Purchases

---
+++ src/components/Tables/PurchaseTable.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const PurchaseTable = () => {
+  return (
+    <div>PurchaseTable</div>
+  )
+}
+
+export default PurchaseTable
--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,8 +1,57 @@
+import { Button, Container, Grid, Typography } from '@mui/material'
 import React from 'react'
+import { useEffect } from 'react';
+import { useState } from 'react';
+import useStockCall from '../hooks/useStockCall';
+import { useSelector } from 'react-redux';
+import ProductModal from '../components/Modals/ProductModal';
 
 const Products = () => {
+
+  const [open, setOpen] = useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => {
+    setOpen(false);
+    setInitialState({
+      name: "",
+      categoryId: "",
+      brandId: "",      
+    })
+  } 
+  
+  const [initialState, setInitialState] = useState({
+    name: "",
+    categoryId: "",
+    brandId: "", 
+  })
+
+  const {getStockData} = useStockCall()
+
+  useEffect(()=>{
+    getStockData("products")
+  },[])
+
+  const {products} = useSelector(state=> state.stock)
+
+  console.log(products);
+  
+
   return (
-    <div>Products</div>
+    <Container>
+      <Typography
+        align="center"
+        color="secondary.second"
+        variant="h4"
+        component="h1"
+      >
+        Products
+      </Typography>
+      <Button variant="contained" onClick={handleOpen}>New Product</Button>
+      {open && <ProductModal open={open} handleClose={handleClose} initialState={initialState}/>}
+      <Grid>
+        something good
+      </Grid>
+    </Container>
   )
 }
 
