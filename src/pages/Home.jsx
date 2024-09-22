ec951f081ffd6b27d72e5a4fe93a8d2d5eef7bec
gokcekemaloglu <gokce.kemaloglu@gmail.com>
2024-09-22 22:50:10 +0300

    Added MUI components and new elements to Home page. Create KpiCards and Update KpiCards with sales and purchases amounts with totalCash

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,25 @@
+import { Container, Typography } from '@mui/material'
 import React from 'react'
+import KpiCards from '../components/KpiCards'
+import Charts from '../components/Charts'
 
 const Home = () => {
+
+  
+
   return (
-    <div>Home</div>
+    <Container>
+      <Typography
+        align="center"
+        color="secondary.second"
+        variant="h4"
+        component="h1"
+      >
+        Dashboard
+      </Typography>
+      <KpiCards/>
+      <Charts/>
+    </Container>
   )
 }
 
